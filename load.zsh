721407b39c3ff3da104af7e41e6a231e118facce
Marcus Albertsson <marcus.arubertoson@gmail.com>
2018-02-19 12:01:46 +0000

    Fix: move pyenv to change PATH at one location

--- load.zsh
+++ load.zsh
@@ -11,8 +11,3 @@ autoload -Uz compinit && compinit -d $ZSH_CACHE/zcompdump
 # TODO: Needs to be better handled
 eval "$(dircolors ${ZDOTDIR}/.dircolors)"
 
-# python
-if [ -x "$(command -v pyenv)" ]; then
-  eval "$(pyenv init -)"
-  eval "$(pyenv virtualenv-init -)"
-fi
