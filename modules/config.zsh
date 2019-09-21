8c77991f5a27646c530ad9f6c3ffa40ffd1c2f26
Marcus Albertsson <marcus.arubertoson@gmail.com>
2019-09-21 21:56:43 +0200

    .

--- modules/config.zsh
+++ modules/config.zsh
@@ -33,7 +33,6 @@ unsetopt CHECK_JOBS       # Don't report on jobs when shell exit.
 # History
 # ----------------------------------------------------------------------------- 
 
-HISTFILE="$XDG_CACHE_HOME/zhistory"
 HISTSIZE=1000                    # Max events to store in internal history.
 SAVEHIST=1000                    # Max events to store in history file.
 
